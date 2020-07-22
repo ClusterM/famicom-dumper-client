cbca7dcda071a4c762a3b82f6650fb1df03c692f
Cluster <clusterrr@clusterrr.com>
2020-07-22 23:51:58 +0300

    Version 2.0

--- FamicomDumper/Properties/AssemblyInfo.cs
+++ FamicomDumper/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FamicomDumper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("FamicomDumperClient")]
+[assembly: AssemblyDescription("Client for Famicom dumper/writer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FamicomDumper")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © Alexey 'Cluster' Avdyukhin, 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
