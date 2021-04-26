b8c47b6c1cfc88e0c77dbe83973b472f583b053e
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2021-04-26 06:07:27 +0300

    Demo script updated

--- FamicomDumper/scripts/DemoScript.cs
+++ FamicomDumper/scripts/DemoScript.cs
@@ -3,15 +3,21 @@
     class DemoScript // Class name also doesn't matter
     {
         // But method signature must be like this. Also you can make this method static.
-        void Run(IFamicomDumperConnection dumper)
+        void Run(IFamicomDumperConnection dumper, string[] args)
         {
-            Console.WriteLine("Please insert MMC3 cartridge and press any key");
-            Console.ReadKey();
+            // You can parse additional command line arguments if need
+            // Specify arguments this way: >FamicomDumper.exe script --csfile DemoScript.cs - argument1 argument2 argument3
+            if (args.Any())
+                Console.WriteLine("Command line arguments: " + string.Join(", ", args));
+
+            Console.WriteLine("Please insert MMC3 cartridge and press enter");
+            Console.ReadLine();
 
             Console.WriteLine("Let's check - how many PRG banks on this MMC3 cartridge");
             byte[] firstBank = null;
             for (var bank = 0; ; bank = bank == 0 ? 1 : bank * 2)
             {
+                if (bank > 256) throw new InvalidDataException("Bank number out of range, did you actually insert the MMC3 cartridge?");
                 Console.Write("Reading PRG bank #{0}... ", bank);
                 dumper.WriteCpu(0x8000, new byte[] { 6, (byte)bank });
                 var data = dumper.ReadCpu(0x8000, 0x2000);
@@ -28,6 +34,7 @@
             Console.WriteLine("Let's check - how many CHR banks on this MMC3 cartridge");
             for (var bank = 0; ; bank = bank == 0 ? 1 : bank * 2)
             {
+                if (bank > 256) throw new InvalidDataException("Bank number out of range, did you actually insert the MMC3 cartridge?");
                 Console.Write("Reading CHR bank #{0}... ", bank);
                 dumper.WriteCpu(0x8000, new byte[] { 2, (byte)bank });
                 var data = dumper.ReadPpu(0x1000, 0x0400);
