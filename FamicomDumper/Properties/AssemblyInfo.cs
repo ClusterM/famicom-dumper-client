09f76bcc369e50068b1ae3ce89bb602b8695bc94
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2021-04-28 12:44:17 +0300

    Updated version number to 2.2.

--- FamicomDumper/Properties/AssemblyInfo.cs
+++ FamicomDumper/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FamicomDumperClient")]
-[assembly: AssemblyDescription("Client for Famicom dumper/writer")]
+[assembly: AssemblyTitle("Famicom Dumper Client")]
+[assembly: AssemblyDescription("Client for Famicom Dumper/Writer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright © Alexey 'Cluster' Avdyukhin, 2020")]
+[assembly: AssemblyCopyright("Copyright © Alexey 'Cluster' Avdyukhin, 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
