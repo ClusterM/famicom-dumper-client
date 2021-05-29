833c08622850423db583bd77eb3ddbfac5c46631
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2021-05-29 11:25:25 +0300

    Updated release.bat

--- relese.bat
+++ relese.bat
@@ -2,20 +2,28 @@ set ARCHS=win-x86 win-x64 win-arm linux-x64 linux-arm osx-x64
 set APP_NAME=famicom-dumper
 set PROJECT_PATH=FamicomDumper
 set OUTPUT_DIR=release
+set CONFIGURATION=Release
 
 set START_DIRECTORY=%CD%
 
-del /F /S /Q %OUTPUT_DIR%
+rmdir /S /Q %OUTPUT_DIR%
+dotnet clean -c %CONFIGURATION%
 
 FOR %%a IN (%ARCHS%) do (
-  dotnet publish %PROJECT_PATH% -c Release -r %%a -p:PublishSingleFile=true --no-self-contained -p:IncludeAllContentForSelfExtract=true -o %OUTPUT_DIR%\%%a\%APP_NAME%
+  dotnet publish %PROJECT_PATH% -c %CONFIGURATION% -r %%a -p:PublishSingleFile=true --no-self-contained -p:IncludeAllContentForSelfExtract=true -o %OUTPUT_DIR%\%%a\%APP_NAME%
+)
+
+FOR %%a IN (%ARCHS%) do (
+  dotnet publish %PROJECT_PATH% -c %CONFIGURATION% -r %%a -p:PublishSingleFile=true --self-contained true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true -o %OUTPUT_DIR%\%%a-self-contained\%APP_NAME%
+)
+
+FOR %%a IN (%ARCHS%) do (
   cd %OUTPUT_DIR%\%%a
   7z a -r -tzip -mx9 ..\%APP_NAME%-%%a.zip %APP_NAME%
   cd %START_DIRECTORY%
 )
 
 FOR %%a IN (%ARCHS%) do (
-  dotnet publish %PROJECT_PATH% -c Release -r %%a -p:PublishSingleFile=true --self-contained true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true -o %OUTPUT_DIR%\%%a-self-contained\%APP_NAME%
   cd %OUTPUT_DIR%\%%a-self-contained
   7z a -r -tzip -mx9 ..\%APP_NAME%-%%a-self-contained.zip %APP_NAME%
   cd %START_DIRECTORY%
