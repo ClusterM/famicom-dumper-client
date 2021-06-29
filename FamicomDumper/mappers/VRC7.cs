c8049b817abdb1cb6dba290e2c4c1b7674c2d0ed
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2021-06-29 21:11:52 +0300

    Script for VRC7

---
+++ FamicomDumper/mappers/VRC7.cs
@@ -0,0 +1,68 @@
+﻿class VRC7 : IMapper
+{
+    public string Name
+    {
+        get { return "VRC7"; }
+    }
+
+    public int Number
+    {
+        get { return 85; }
+    }
+
+    public byte Submapper
+    {
+        get { return 0; }
+    }
+
+    public string UnifName
+    {
+        get { return null; }
+    }
+
+    public int DefaultPrgSize
+    {
+        get { return 512 * 1024; }
+    }
+
+    public int DefaultChrSize
+    {
+        get { return 256 * 1024; }
+    }
+
+    public void DumpPrg(IFamicomDumperConnection dumper, List<byte> data, int size)
+    {
+        var banks = size / 0x2000;
+
+        for (var bank = 0; bank < banks; bank++)
+        {
+            Console.Write($"Reading PRG bank #{bank}/{banks}... ");
+            dumper.WriteCpu(0x8000, (byte)bank);
+            data.AddRange(dumper.ReadCpu(0x8000, 0x2000));
+            Console.WriteLine("OK");
+        }
+    }
+
+    public void DumpChr(IFamicomDumperConnection dumper, List<byte> data, int size)
+    {
+        var banks = size / 0x400;
+
+        for (var bank = 0; bank < banks; bank++)
+        {
+            Console.Write($"Reading CHR bank #{bank}/{banks}... ");
+            dumper.WriteCpu(0xA000, (byte)bank);
+            data.AddRange(dumper.ReadPpu(0x0000, 0x0400));
+            Console.WriteLine("OK");
+        }
+    }
+
+    public void EnablePrgRam(IFamicomDumperConnection dumper)
+    {
+        dumper.WriteCpu(0xE000, 0x80);
+    }
+
+    public NesFile.MirroringType GetMirroring(IFamicomDumperConnection dumper)
+    {
+        return NesFile.MirroringType.MapperControlled;
+    }
+}
