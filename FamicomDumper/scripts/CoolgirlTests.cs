67bed0c09022ba4eacd6fef029f6a76a57a240a0
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2021-07-13 16:40:11 +0300

    Updated COOLGIRL tests script

--- FamicomDumper/scripts/CoolgirlTests.cs
+++ FamicomDumper/scripts/CoolgirlTests.cs
@@ -76,23 +76,30 @@ class CoolgirlTests
         Console.WriteLine("OK");
         dumper.WriteCpu(0x5007, 0x01); // enable PRG RAM
         var rnd = new Random();
+        int bankSize = 0x2000;
+        int banks = 4;
         while (count != 0)
         {
-            var data = new byte[][] { new byte[0x2000], new byte[0x2000], new byte[0x2000], new byte[0x2000] };
-            for (byte bank = 0; bank < 4; bank++)
+            var data = new byte[banks][];
+            for (int i = 0; i < banks; i++)
+            {
+                data[i] = new byte[0x2000];
+                rnd.NextBytes(data[i]);
+            }
+            for (byte bank = 0; bank < banks; bank++)
             {
                 Console.WriteLine($"Writing PRG RAM, bank #{bank}/{4}... ");
                 rnd.NextBytes(data[bank]);
                 dumper.WriteCpu(0x5005, bank);
                 dumper.WriteCpu(0x6000, data[bank]);
             }
-            for (byte bank = 0; bank < 4; bank++)
+            for (byte bank = 0; bank < banks; bank++)
             {
                 Console.Write($"Reading PRG RAM, bank #{bank}/{4}... ");
                 dumper.WriteCpu(0x5005, bank);
-                var rdata = dumper.ReadCpu(0x6000, 0x2000);
+                var rdata = dumper.ReadCpu(0x6000, bankSize);
                 bool ok = true;
-                for (int b = 0; b < 0x2000; b++)
+                for (int b = 0; b < rdata.Length; b++)
                 {
                     if (data[bank][b] != rdata[b])
                     {
@@ -102,6 +109,7 @@ class CoolgirlTests
                 }
                 if (!ok)
                 {
+                    DetectProblems(data[bank], rdata, "CPU");
                     File.WriteAllBytes("prgramgood.bin", data[bank]);
                     Console.WriteLine("prgramgood.bin writed");
                     File.WriteAllBytes("prgrambad.bin", rdata);
@@ -122,66 +130,43 @@ class CoolgirlTests
         Console.WriteLine("OK");
         dumper.WriteCpu(0x5007, 0x2); // enable CHR writing
         var rnd = new Random();
-        var data = new byte[0x2000];
-        rnd.NextBytes(data);
-        Console.WriteLine("Single bank test.");
-        Console.Write("Writing CHR RAM... ");
-        dumper.WritePpu(0x0000, data);
-        Console.Write("Reading CHR RAM... ");
-        var rdata = dumper.ReadPpu(0x0000, 0x2000);
-        bool ok = true;
-        for (int b = 0; b < 0x2000; b++)
-        {
-            if (data[b] != rdata[b])
-            {
-                Console.WriteLine($"Mismatch at {b:X4}: {rdata[b]:X2} != {data[b]:X2}");
-                ok = false;
-            }
-        }
-        if (!ok)
-        {
-            File.WriteAllBytes("chrramgood.bin", data);
-            Console.WriteLine("chrramgood.bin writed");
-            File.WriteAllBytes("chrrambad.bin", rdata);
-            Console.WriteLine("chrrambad.bin writed");
-            throw new IOException("Test failed");
-        }
-        Console.WriteLine("OK");
-
-        Console.WriteLine("Multibank test.");
-        data = new byte[chrSize];
+        int bankSize = 0x2000;
+        int banks = chrSize / bankSize;
         for (; count != 0; count--)
         {
-            dumper.WriteCpu(0x5007, 0x2); // enable CHR writing
-            rnd.NextBytes(data);
-            for (byte bank = 0; bank < data.Length / 0x2000; bank++)
+            var data = new byte[banks][];
+            for (int i = 0; i < data.Length; i++)
             {
-                Console.WriteLine($"Writing CHR RAM bank #{bank}/{data.Length / 0x2000}...");
-                dumper.WriteCpu(0x5003, (byte)(bank & 0b00011111)); // select bank, low 5 bits
-                dumper.WriteCpu(0x5005, (byte)((bank & 0b00100000) << 2)); // select bank, 6th bit
-                var d = new byte[0x2000];
-                Array.Copy(data, bank * 0x2000, d, 0, 0x2000);
-                dumper.WritePpu(0x0000, d);
+                data[i] = new byte[0x2000];
+                rnd.NextBytes(data[i]);
             }
-            for (byte bank = 0; bank < data.Length / 0x2000; bank++)
+            for (byte bank = 0; bank < banks; bank++)
             {
-                Console.Write($"Reading CHR RAM bank #{bank}/{data.Length / 0x2000}... ");
+                Console.WriteLine($"Writing CHR RAM bank #{bank}/{banks}...");
                 dumper.WriteCpu(0x5003, (byte)(bank & 0b00011111)); // select bank, low 5 bits
                 dumper.WriteCpu(0x5005, (byte)((bank & 0b00100000) << 2)); // select bank, 6th bit
-                rdata = dumper.ReadPpu(0x0000, 0x2000);
-                ok = true;
-                for (int b = 0; b < 0x2000; b++)
+                dumper.WritePpu(0x0000, data[bank]);
+            }
+            for (byte bank = 0; bank < banks; bank++)
+            {
+                Console.Write($"Reading CHR RAM bank #{bank}/{banks}... ");
+                dumper.WriteCpu(0x5003, (byte)(bank & 0b00011111)); // select bank, low 5 bits
+                dumper.WriteCpu(0x5005, (byte)((bank & 0b00100000) << 2)); // select bank, 6th bit
+                var rdata = dumper.ReadPpu(0x0000, bankSize);
+                var ok = true;
+                for (int b = 0; b < rdata.Length; b++)
                 {
-                    if (data[b + bank * 0x2000] != rdata[b])
+                    if (data[bank][b] != rdata[b])
                     {
-                        Console.WriteLine($"Mismatch at {b:X4}: {rdata[b]:X2} != {data[b + bank * 0x2000]:X2}");
+                        Console.WriteLine($"Mismatch at {b:X4}: {rdata[b]:X2} != {data[bank][b]:X2}");
                         ok = false;
                     }
                 }
                 if (!ok)
                 {
-                    File.WriteAllBytes("chrramgoodf.bin", data);
-                    Console.WriteLine("chrramgoodf.bin writed");
+                    DetectProblems(data[bank], rdata, "PPU");
+                    File.WriteAllBytes("chrramgood.bin", data[0]);
+                    Console.WriteLine("chrramgood.bin writed");
                     File.WriteAllBytes("chrrambad.bin", rdata);
                     Console.WriteLine("chrrambad.bin writed");
                     throw new InvalidDataException("Test failed");
@@ -189,5 +174,41 @@ class CoolgirlTests
                 Console.WriteLine("OK");
             }
         }
+    }
+
+    // Function to detect problem lines
+    private static void DetectProblems(byte[] good, byte[] bad, string busName)
+    {
+        var size = Math.Min(good.Length, bad.Length);
+        int problemBits = 0;
+        for (int i = 0; i < size; i++)
+        {
+            problemBits |= (byte)(good[i] ^ bad[i]);
+        }
+        if (problemBits != 0xFF)
+        {
+            for (int i = 0; i < 8; i++)
+                if ((problemBits & (1 << i)) != 0)
+                    Console.WriteLine($"Problems on line D{i} @ {busName}");
+        }
+
+        int memoryTestSize = 4;
+        problemBits = 0;
+        for (int i = memoryTestSize; i < bad.Length; i += memoryTestSize)
+        {
+            bool matched = true;
+            for (int j = 0; j < memoryTestSize; j++)
+            {
+                if (bad[i + j] != bad[j])
+                {
+                    matched = false;
+                    break;
+                }
+            }
+            if (matched) problemBits |= i;
+        }
+        for (int i = 0; i < 16; i++)
+            if ((problemBits & (1 << i)) != 0)
+                Console.WriteLine($"Problems on line A{i} @ {busName}");
     }
 }
