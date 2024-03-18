18c3b042cd6fe54a1b7e205f9ff50c956d4458d1
Alexey Cluster <cluster@cluster.wtf>
2024-03-18 23:04:45 +0400

    GTROM mapper

---
+++ FamicomDumper/mappers/GTROM.cs
@@ -0,0 +1,23 @@
+﻿class GTROM : IMapper
+{
+    public string Name { get => "GTROM"; }
+    public int Number { get => 111; }
+    public int DefaultPrgSize { get => 512 * 1024; }
+
+    public void DumpPrg(IFamicomDumperConnection dumper, List<byte> data, int size)
+    {
+        var banks = size / 0x8000;
+        for (var bank = 0; bank < banks; bank++)
+        {
+            Console.Write($"Reading PRG bank #{bank}/{banks}... ");
+            dumper.WriteCpu(0x5000, (byte)bank);
+            data.AddRange(dumper.ReadCpu(0x8000, 0x8000));
+            Console.WriteLine("OK");
+        }
+    }
+
+    public MirroringType GetMirroring(IFamicomDumperConnection dumper)
+    {
+        return MirroringType.FourScreenVram;
+    }
+}
